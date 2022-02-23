c5c67a053968cb0810edaf760a2d51eec5137bc8
Jack Evans <jackevans24@googlemail.com>
2022-02-23 11:35:55 +0000

    Add offset option to spike generator

--- Assets/Scripts/SpikeGenerator.cs
+++ Assets/Scripts/SpikeGenerator.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 public class SpikeGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject spike;
+    [SerializeField] private float startOffset = 0f;
     [SerializeField] private float spawnEvery = 3f;
 
+    private bool offsetReached;
+    private float currentOffsetTimer;
     private float currentSpawnTimer;
 
     void Update()
     {
+        if (!offsetReached)
+        {
+            this.currentOffsetTimer += Time.deltaTime;
+            if (this.currentOffsetTimer >= this.startOffset)
+                this.offsetReached = true;
+
+            return;
+        }
+
         this.currentSpawnTimer += Time.deltaTime;
         if (this.currentSpawnTimer >= this.spawnEvery)
         {
