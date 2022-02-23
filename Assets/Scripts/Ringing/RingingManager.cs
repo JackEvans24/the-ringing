cd0953f574933dc788028145fe813488a30b23b0
Jack Evans <jackevans24@googlemail.com>
2022-02-23 15:26:36 +0000

    Add level end ringing

--- Assets/Scripts/Ringing/RingingManager.cs
+++ Assets/Scripts/Ringing/RingingManager.cs
@@ -1,7 +1,9 @@
+using Assets.Scripts.Helpers;
 using DG.Tweening;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class RingingManager : MonoBehaviour
 {
@@ -21,13 +23,14 @@ public class RingingManager : MonoBehaviour
     [SerializeField] private float moveDistance = 2f;
     [SerializeField] private float moveTime = 2f;
     [SerializeField] private Ease moveEasing = Ease.InOutSine;
+    [SerializeField] private UnityEvent onRing;
 
     [Header("Activation")]
     [SerializeField] private bool ringWithoutPrompt;
     [SerializeField] private float ringEvery = 5f;
 
     private Transform currentRingTransform;
-    private bool ringing;
+    private bool ringing, endOfLevel;
     private float currentRingTimer;
     private Vector2 ringPointOnBounds;
 
@@ -43,6 +46,9 @@ public class RingingManager : MonoBehaviour
 
     private void Update()
     {
+        if (this.endOfLevel)
+            return;
+
         if (!this.ringWithoutPrompt)
         {
             this.currentRingTimer = 0f;
@@ -67,27 +73,31 @@ public class RingingManager : MonoBehaviour
         instance.StartCoroutine(instance.RingAnimation());
     }
 
+    public static void EndOfLevel(Transform crystal)
+    {
+        instance.endOfLevel = true;
+        instance.currentRingTransform = crystal;
+
+        Ring();
+    }
+
     private IEnumerator RingAnimation()
     {
         if (this.ringing)
             yield break;
         this.ringing = true;
 
-        var bounds = RingHelpers.GetCameraSize(this.mainCamera);
-        var (withinBounds, ringPoint) = RingHelpers.GetPointWithinBounds(bounds, this.currentRingTransform.position, this.border);
-        if (withinBounds)
+        var (startPosition, endPosition) = this.GetTextPositions();
+        if (startPosition == Vector2.zero && endPosition == Vector2.zero)
         {
             this.ringing = false;
             yield break;
         }
 
-        var startPosition = new Vector2(ringPoint.x / canvas.localScale.x, ringPoint.y / canvas.localScale.y);
-
-        var moveDirection =  (bounds.center - startPosition).normalized * this.moveDistance;
-        var endPosition = startPosition + moveDirection;
-
         this.ringPointOnBounds = startPosition;
         this.ringingText.transform.localPosition = startPosition;
+
+        this.onRing?.Invoke();
 
         var moveTween = this.ringingText.transform
             .DOLocalMove(endPosition, this.moveTime)
@@ -110,8 +120,42 @@ public class RingingManager : MonoBehaviour
         this.ringing = false;
     }
 
+    private (Vector2 startPosition, Vector2 endPosition) GetTextPositions()
+    {
+        Vector2 startPosition;
+        Vector2 endPosition;
+
+        if (this.endOfLevel)
+        {
+            startPosition = this.currentRingTransform.position + (Vector2.up * this.border).ToVector3();
+            endPosition = startPosition + Vector2.up * moveDistance;
+        }
+        else
+        {
+            var bounds = RingHelpers.GetCameraSize(this.mainCamera);
+            var (withinBounds, ringPoint) = RingHelpers.GetPointWithinBounds(bounds, this.currentRingTransform.position, this.border);
+            if (withinBounds)
+            {
+                this.ringing = false;
+                return (Vector2.zero, Vector2.zero);
+            }
+
+            startPosition = new Vector2(ringPoint.x / canvas.localScale.x, ringPoint.y / canvas.localScale.y);
+
+            var moveDirection = (bounds.center - startPosition).normalized * this.moveDistance;
+            endPosition = startPosition + moveDirection;
+        }
+
+        return (startPosition, endPosition);
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawSphere(this.ringPointOnBounds, 0.5f);
+    }
+
+    private void OnDestroy()
+    {
+        instance = null;
     }
 }
